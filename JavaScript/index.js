ec1c28cc1192a90ab7365c71878cacd13a4d9597
YolandaCR <yolandacorderorobledo@gmail.com>
2021-03-03 20:58:31 +0100

    vuelvo a poner los js

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+﻿// seccion de codigo javascript
+
+		function guardaUsr() {
+			// guardo la variable nomUsr con el usuario ingresado en el formulario
+			sessionStorage.setItem("nomUsr", document.forms["miFormulario"]["usuario"].value);
+			// carga la pagina de logout...
+			window.open("Logout.html");
+		}
