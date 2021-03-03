c27722c2725fdbf79476dafaedae166d8708fc2a
YolandaCR <yolandacorderorobledo@gmail.com>
2021-03-03 21:47:23 +0100

    ULTIMA ACTUALIZACION MIERCOLES 3/3

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,7 @@
 ﻿// seccion de codigo javascript
-
 		function guardaUsr() {
 			// guardo la variable nomUsr con el usuario ingresado en el formulario
 			sessionStorage.setItem("nomUsr", document.forms["miFormulario"]["usuario"].value);
 			// carga la pagina de logout...
-			window.open("Logout.html","_self");
+			window.open("Logout.html");
 		}
--- JavaScript/logout.js
+++ JavaScript/logout.js
@@ -2,10 +2,9 @@
 		// la siguiente linea recupera el valor guardado previamente
 		// con sessionStorage funciona solo recargando, con localStorage incluso apagando el PC
 		document.getElementById("aqui").innerHTML = sessionStorage.getItem("nomUsr");
-
 		function borraUsr() {
 			// elimino la variable nomUsr
 			sessionStorage.removeItem("nomUsr");
 			// cargar la pagina de login ...
-            window.open("index.html","_self");
+            window.open("index.html");
 		}
