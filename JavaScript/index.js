19db4c3d0140999a53487f51e2ce75b3b480163b
YolandaCR <yolandacorderorobledo@gmail.com>
2021-03-03 21:01:37 +0100

    misma pestaña

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,5 +4,5 @@
 			// guardo la variable nomUsr con el usuario ingresado en el formulario
 			sessionStorage.setItem("nomUsr", document.forms["miFormulario"]["usuario"].value);
 			// carga la pagina de logout...
-			window.open("Logout.html");
+			window.open("Logout.html","_self");
 		}
--- JavaScript/logout.js
+++ JavaScript/logout.js
@@ -7,6 +7,6 @@
 			// elimino la variable nomUsr
 			sessionStorage.removeItem("nomUsr");
 			// cargar la pagina de login ...
-            window.open("index.html");
+            window.open("index.html","_self");
 
 		}
