140c43cbd851f7d7c8821c06a89cf287692fb817
YolandaCR <yolandacorderorobledo@gmail.com>
2021-03-03 16:16:08 +0100

    actualizacion antes de tocar algo importante

--- JavaScript/logout.js
+++ JavaScript/logout.js
@@ -8,7 +8,7 @@
 			// elimino la variable nomUsr
 			sessionStorage.removeItem("nomUsr");
 			// cargar la pagina de login ...
-            window.open("indexT2.html");
+            window.open("index.html");
 
 		}
 
