9a976c2dfa74a04ab21707af138489e32143a0ff
YolandaCR <yolandacorderorobledo@gmail.com>
2021-03-04 11:41:10 +0100

    Actualizacion 4 marzo
    Actualización con lo trabajado en la web hoy

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,5 +3,6 @@
 			// guardo la variable nomUsr con el usuario ingresado en el formulario
 			sessionStorage.setItem("nomUsr", document.forms["miFormulario"]["usuario"].value);
 			// carga la pagina de logout...
-			window.open("Logout.html");
+			window.open("./html/Logout.html");
+			window.close();
 		}
